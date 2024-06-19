55f72151c2fbe78031cc87f4a482ef66e133a884
Ali-Khaled-dev <ali.khaled.dev@gmail.com>
2024-06-19 12:59:27 +0300

    add constract and functions

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,86 +2,46 @@
 
 namespace App\Http\Controllers\Api;
 
-
-use App\Http\Controllers\Api\ApiResponseTrait;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
-use App\Http\Resources\CategoryResource;
-use App\Models\Category;
+use App\Repositry\CategoryInterface;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    use ApiResponseTrait;
-    
+    protected $category;
+
+    public function __construct(CategoryInterface $category)
+    {
+        $this->category = $category;
+    }
+
     public function index()
     {
-        $categories = CategoryResource::collection(Category::all());
-
-        return $this->apiResponse($categories, 'succsess' , 200);
+        return $this->category->index();
     }
 
-    
+
     public function store(CategoryRequest $request)
     {
-     
-       $category = Category::create([
-
-           'en' => ['name' => $request -> en_name],
-           'ar' => ['name' => $request -> ar_name],
-
-           'slug' => $request -> slug,
-        ]);
-
-        return $this->apiResponse(new CategoryResource($category), __('succsess') , 200);
+        return $this->category->store($request);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        $category = Category::findOrFail($id);
-
-        if(!$category)
-        {
-        
-            return $this -> apiResponse(null, __('The Category not found') , 404);
-
-        }
-
-        return $this -> apiResponse(new CategoryResource($category), __('succsess') , 200);
-
+        return $this->category->show($id);
     }
 
-    
+
     public function update(CategoryRequest $request, string $id)
     {
-        $category= Category::findOrFail($id);
-
-        $category -> en_name = $request -> en_name;
-        $category -> ar_name = $request -> ar_name;
-        $category -> slug = $request -> slug;
-       
- 
-         return $this->apiResponse($category, __('succsess') , 200);
+        return $this->category->update($request, $id);
     }
 
-   
+
     public function destroy(string $id)
     {
-        $post = Category::findOrFail($id);
-
-        if(!$post)
-        {
-
-            return $this->apiResponse(null, __('The Category Not Found') , 404);
-        
-        }
-
-        $post->delete();
-
-            return $this->apiResponse(null, __('success') , 200);
-        
+        return $this->category->destroy($id);
     }
 }
