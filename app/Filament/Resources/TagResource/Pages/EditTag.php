fa77393789522de2abc13a192210acc734e3aada
Ali-Khaled-dev <ali.khaled.dev@gmail.com>
2024-07-31 16:54:36 +0300

    add traits

--- app/Filament/Resources/TagResource/Pages/EditTag.php
+++ app/Filament/Resources/TagResource/Pages/EditTag.php
@@ -4,9 +4,12 @@ namespace App\Filament\Resources\TagResource\Pages;
 
 use App\Filament\Resources\TagResource;
 
+use App\Filament\Traits\RedirectUrlTrait;
+use App\Filament\Traits\Translatable;
 use Filament\Resources\Pages\EditRecord;
 
 class EditTag extends EditRecord
 {
+    use Translatable, RedirectUrlTrait;
     protected static string $resource = TagResource::class;
 }
