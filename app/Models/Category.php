9370d1df7aa9433b5893274b8aa5a78c4fc93acb
Ali-Khaled-dev <ali.khaled.dev@gmail.com>
2024-09-29 02:53:40 +0300

    costumize model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,23 +3,27 @@
 namespace App\Models;
 
 use App\Filament\Traits\InputsTrait;
-use App\Filament\Traits\RedirectUrlTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
-use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Tabs;
-use Filament\Forms\Components\TextInput;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Filament\Forms\Set;
 
 class Category extends Model implements TranslatableContract
 {
     use HasFactory, Translatable;
 
 
-    public $translatedAttributes = ['name'];
+    public $translatedAttributes = [
+        'locale',
+        'name',
+        'slug',
+        'meta_descreption',
+        'meta_keywords',
+    ];
 
     public function articals()
     {
@@ -35,20 +39,50 @@ class Category extends Model implements TranslatableContract
                 ->schema([
 
                     Tabs::make('Tabs')
-                        ->tabs(fn () => array_map(
-                            fn ($language, $locale) => Tabs\Tab::make($language['native'])->schema([
-                                InputsTrait::input(
-                                    $locale . '.name',
-                                    __('Name') . '(' . $language['native'] . ')',
-                                    __('Enter Name'),
-                                    'heroicon-m-language'
-                                )
-                                    ->maxLength(50),
+                        ->tabs(fn() => array_map(
+                            fn($language, $locale) => Tabs\Tab::make($language['native'])->schema([
+                                Section::make()->schema([
+                                    InputsTrait::input(
+                                        $locale . '.name',
+                                        __('Name') . '(' . $language['native'] . ')',
+                                        __('Enter Name'),
+                                        'heroicon-m-language'
+                                    )
+                                        ->maxLength(50)->live(onBlur: true)
+                                        ->afterStateUpdated(function (Set $set, string $state) use ($locale) {
+                                            $set($locale . '.slug', InputsTrait::formatText($state));
+                                        }),
+
+                                    InputsTrait::input(
+                                        $locale . '.slug',
+                                        __('Slug') . '(' . $language['native'] . ')',
+                                        __('Enter Slug'),
+                                        'heroicon-m-language'
+                                    )->readOnly(),
+
+                                ])->columnSpan(2),
+
+                                Section::make('meta')->label('Meta')->schema([
+
+                                    InputsTrait::tags(
+                                        $locale . '.meta_descreption',
+                                        __('Meta Descreption') . '(' . $language['native'] . ')',
+                                        __('Enter Name'),
+                                        'heroicon-m-language'
+                                    ),
+                                    InputsTrait::tags(
+                                        $locale . '.meta_keywords',
+                                        __('Meta KeyWords') . '(' . $language['native'] . ')',
+                                        __('Enter Name'),
+                                        'heroicon-m-language'
+                                    ),
+                                ])->columnSpan(2),
                             ]),
+
 
                             LaravelLocalization::getLocalesOrder(),
                             array_keys(LaravelLocalization::getLocalesOrder())
-                        ))->columnSpanFull(),
+                        ))->columns(4),
                 ]),
         ];
     }
