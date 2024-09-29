e5ef8a997bc1c3aa39c955fdc35fbf237a305a40
Ali-Khaled-dev <ali.khaled.dev@gmail.com>
2024-09-29 03:00:44 +0300

    RedirectUrlTrait

--- app/Filament/Resources/ArticalResource/Pages/EditArtical.php
+++ app/Filament/Resources/ArticalResource/Pages/EditArtical.php
@@ -3,11 +3,12 @@
 namespace App\Filament\Resources\ArticalResource\Pages;
 
 use App\Filament\Resources\ArticalResource;
+use App\Filament\Traits\RedirectUrlTrait;
 use App\Filament\Traits\Translatable;
 use Filament\Resources\Pages\EditRecord;
 
 class EditArtical extends EditRecord
 {
-    use Translatable;
+    use Translatable,RedirectUrlTrait;
     protected static string $resource = ArticalResource::class;
 }
--- app/Filament/Resources/AuthorResource/Pages/EditAuthor.php
+++ app/Filament/Resources/AuthorResource/Pages/EditAuthor.php
@@ -3,10 +3,11 @@
 namespace App\Filament\Resources\AuthorResource\Pages;
 
 use App\Filament\Resources\AuthorResource;
-
+use App\Filament\Traits\RedirectUrlTrait;
 use Filament\Resources\Pages\EditRecord;
 
 class EditAuthor extends EditRecord
 {
+    use RedirectUrlTrait;
     protected static string $resource = AuthorResource::class;
 }
